0bfd4249fcef9c980035b81d3013dcf4d83fa971
BranHu <348090150@qq.com>
2017-04-25 23:33:20 +0800

    变更了太多 webpack

--- reactjs/react-redux-webpack-babel/webpack.config.js
+++ reactjs/react-redux-webpack-babel/webpack.config.js
@@ -1,31 +1,65 @@
-var path = require('path');
+var { resolve } = require('path');
 var webpack = require('webpack');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
 //常用路径
-var ROOT_PATH = path.resolve(__dirname);
-var APP_PATH = path.resolve(ROOT_PATH,'app');
-var BUILD_PATH = path.resolve(ROOT_PATH,'build');
+var ROOT_PATH = resolve(__dirname);
+var APP_PATH = resolve(ROOT_PATH, 'app');
+var BUILD_PATH = resolve(ROOT_PATH, 'build');
 
 module.exports = {
-	entry: {
-		app: path.resolve(APP_PATH,'index.jsx')
-	},
+	//改动1.增加webpack的context属性
+	//The base directory, an absolute path, for resolving entry points and loaders from configuration.
+	context: resolve(__dirname, 'src'),
+	//改动2.entry进行了较大的改动
+	/*entry: {
+		app: resolve(APP_PATH, 'index.jsx')
+	},*/
+	entry: [
+		'react-hot-loader/patch',
+	    // activate HMR for React
+
+	    'webpack-dev-server/client?http://localhost:8080',
+	    // bundle the client for webpack-dev-server
+	    // and connect to the provided endpoint
+
+	    'webpack/hot/only-dev-server',
+	    // bundle the client for hot reloading
+	    // only- means to only hot reload for successful updates
+
+	    './app.js'
+	    // the entry point of our app
+    ],
 	output: {
 		path: BUILD_PATH,
 		filename: 'bundle.js'
+		//publicPath: '/'
+        // necessary for HMR to know where to load the hot update chunks
+        //生产环境即上线才会用到publicPath-----CDN
 	},
 
-	//开启dev source map
-	devtool: 'eval-source-map',
+	//改动3.开启dev source map
+	//devtool: 'eval-source-map',
+	devtool: 'inline-source-map',
 	//开启webpack dev server
+
+	//改动4.删减了部分属性,但同时增加了部分属性
 	devServer: {
-		historyApiFallback: true,
-		hot: true,
+		/*historyApiFallback: true,
+		hot:true,
 		inline: true,
-		progress: true
+		progress: true*/
+		hot: true,
+    	// enable HMR on the server
+
+	    contentBase: resolve(__dirname, 'dist'),
+	    // match the output path
+
+	    publicPath: '/'
+	    // match the output `publicPath`
 	},
 
 	module: {
+		//改动5.loaders换成了rules
 		//这是webpack1.0中的方式，会报错，要使用webpack2.0
 		//配置preLoaders,将eslint添加进入.
 		// preLoaders: [
@@ -43,31 +77,39 @@ module.exports = {
 		// 		include: APP_PATH
 		// 	}
 		// ]
-		rules: [
-		    {
-		        test: /\.js$/,
-		        enforce: "pre",
-		        loader: "eslint-loader",
-		        include: APP_PATH
-		    },
-		    {
-		    	test: /\.js$/,
-		        loader: "babel-loader",
-		        include: APP_PATH
-		    }
-    	]
+		rules: [{
+				enforce: "pre",
+				test: /\.js$/,
+				loader: "eslint-loader",
+				exclude: /node_modules/,
+			}, {
+				test: /\.js$/,
+				loader: "babel-loader",
+				exclude: /node_modules/,
+			}, {
+				test: /\.css$/,
+		        use: [
+			        'style-loader',
+			        'css-loader?modules',
+			        'postcss-loader',
+			    ]
+			}
+		]
 
 	},
 
-	//配置plugin
+	////改动6.配置plugin
 	plugins: [
 		new HtmlwebpackPlugin({
 			title: 'My first react app'
-		})
+		}),
+		new webpack.HotModuleReplacementPlugin(),
+
+		new webpack.NamedModulesPlugin()
 	],
 
-	//在js中import加载JXS扩展名的脚本
+	//在js中import加载JSX扩展名的脚本
 	resolve: {
-		extensions: ['.js','.jsx']
+		extensions: [".js", ".jsx"]
 	}
 }
