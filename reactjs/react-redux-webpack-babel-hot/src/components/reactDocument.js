4c0243192425524eb5521974db0573683329de2f
BranHu <348090150@qq.com>
2017-05-01 16:17:44 +0800

    react

---
+++ reactjs/react-redux-webpack-babel-hot/src/components/reactDocument.js
@@ -0,0 +1,160 @@
+import React from 'react';
+
+class ProductCategoryRow extends React.Component {
+  render() {
+    return <tr><th colSpan="2">{this.props.category}</th></tr>;
+  }
+}
+
+
+//注意这里的style引用
+class ProductRow extends React.Component {
+  render() {
+    var name = this.props.product.stocked ?
+      this.props.product.name :
+      <span style={{color: 'red'}}>
+        {this.props.product.name}
+      </span>;
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{this.props.product.price}</td>
+      </tr>
+    );
+  }
+}
+
+class ProductTable extends React.Component {
+  render() {
+    var rows = [];
+    var lastCategory = null;
+
+    //下面的products属性通过this.props来引用，this代表下面的ProductTable的实例,
+    //他有props这个属性，实例中的所有特征属性都在props属性上，因此通过props来引用,
+    //这里要使用箭头函数，不然this会乱
+    this.props.products.forEach((product) => {
+      //设置了state属性后，对相应处进行添加修改
+  	  if (product.name.indexOf(this.props.filterText) === -1 || (!product.stocked && this.props.inStockOnly)) {
+  	      return;
+  	  }
+
+      if (product.category !== lastCategory) {
+        rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+      }
+      rows.push(<ProductRow product={product} key={product.name} />);
+      lastCategory = product.category;
+    });
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+}
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleFilterTextInputChange = this.handleFilterTextInputChange.bind(this);
+    this.handleInStockInputChange = this.handleInStockInputChange.bind(this);
+  }
+
+  //实时控制主要就是在组件中constructor和render()之间新增方法，再通过setState()
+  //来对state中的属性进行更改
+  handleFilterTextInputChange(e) {
+    //this即后面的<SearchBar />，这个实例组件的很多属性如filterText、onFilterTextInput、onInStockInput等都在
+    //this.props属性下面，这里相当于调用了后面的handleFilterTextInput()这个函数
+    this.props.onFilterTextInput(e.target.value);
+  }
+  
+  handleInStockInputChange(e) {
+    this.props.onInStockInput(e.target.checked);
+  }
+
+  render() {
+    return (
+      <form>
+        <input 
+          type="text" 
+          placeholder="Search..." 
+          value={this.props.filterText} 
+          onChange={this.handleFilterTextInputChange}
+        />
+        <p>
+          <input 
+            type="checkbox" 
+            checked={this.props.inStockOnly} 
+            onChange={this.handleInStockInputChange}
+          />
+          {' '}
+          Only show products in stock
+        </p>
+      </form>
+    );
+  }
+}
+
+class FilterableProductTable extends React.Component {
+  //props可以省略，因其默认存在的
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: 'ball',
+      inStockOnly: false
+    };
+    //实时控制的时候添加的
+    this.handleFilterTextInput = this.handleFilterTextInput.bind(this);
+    this.handleInStockInput = this.handleInStockInput.bind(this);
+  }
+
+  //实时控制主要就是在组件中constructor和render()之间新增方法，再通过setState()
+  //来对state中的属性进行更改
+  handleFilterTextInput(filterText) {
+    this.setState({
+      filterText: filterText
+    });
+  }
+  
+  handleInStockInput(inStockOnly) {
+    this.setState({
+      inStockOnly: inStockOnly
+    })
+  }
+
+  render() {
+  	var PRODUCTS = [
+	  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+	  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+	  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+	  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+	  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+	  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+	];
+
+    //this.state只在声明了state的类中使用，其他的用props
+    return (
+      <div>
+        <SearchBar 
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+          onFilterTextInput={this.handleFilterTextInput}
+          onInStockInput={this.handleInStockInput}
+        />
+        <ProductTable 
+          products={PRODUCTS} 
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+        />
+      </div>
+    );
+  }
+}
+
+ 
+export default FilterableProductTable;
--- reactjs/react-redux-webpack-babel-hot/src/index.js
+++ reactjs/react-redux-webpack-babel-hot/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 // AppContainer is a necessary wrapper component for HMR
 import App from './components/app';
+import FilterableProductTable from './components/reactDocument';
 
 // function Welcome(props) {
 // 	return <h1>hello,{props.name}</h1>;
@@ -29,7 +30,7 @@ const render = (Component) => {
 };
 
 //invoke the render()
-render(App);
+render(FilterableProductTable);
 
 // Hot Module Replacement API
 //不要可行？？需要确定,先暂时不用
