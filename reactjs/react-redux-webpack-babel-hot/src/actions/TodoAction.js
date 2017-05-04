ab92d8ae1feef49b04343712e1786cdd5de19664
BranHu <348090150@qq.com>
2017-05-04 17:06:26 +0800

    flux 学习

--- reactjs/react-redux-webpack-babel-hot/src/actions/TodoAction.js
+++ reactjs/react-redux-webpack-babel-hot/src/actions/TodoAction.js
@@ -1,17 +1,24 @@
 import AppDispatcher from './dispatcher/AppDispatcher';
 
 const TodoAction = {
+	//这里有两个action，需要store进行判断(store中的switch)
 	create(todo) {
 		AppDispatcher.dispatch({
 			actionType: 'CREATE_TODO',
-			todo
+			todo,
+			text,
+			value
+		//后面的数据可以有多个
 		});
 	},
 
 	delete(id) {
 		AppDispatcher.dispatch({
 			actionType: 'DELETE_TODO',
-			id
+			id,
+			name,
+			menu
+		//后面的数据可以有多个
 		})
 	}
 }
--- reactjs/react-redux-webpack-babel-hot/src/stores/TodoStore.js
+++ reactjs/react-redux-webpack-babel-hot/src/stores/TodoStore.js
@@ -1,4 +1,5 @@
 import AppDispatcher from './dispatcher/AppDispatcher';
+import { EventEmitter } from 'event';
 
 const TodoStore = {
 	//存放所有文章的列表，里面有两条默认的数据
@@ -16,19 +17,38 @@ const TodoStore = {
 
 	addTodo(todo) {
 		this.todos.push(todo);
-	}
+	},
 
 	deleteTodo(id) {
 		this.todos = this.todos.filter(item => item.id !== id);
-	}
+	},
+  
+    // 触发 change 事件
+    emitChange: function() {
+    	this.emit('change');
+    },
+  
+    // 提供给外部 View 绑定 change 事件
+    // callback 在 View 组件中是声明的onchange方法，它的内部就是调用了setState 方法对state进行了重新设置 
+    addChangeListener: function(callback) {
+    	this.on('change', callback);
+    },
+  
+    removeChangeListener: function() {
+    	this.remove('change', callback);
+    }
+
 };
 
 AppDispatcher.register((action) => {
+
 	switch (action.actionType) {
+
 		case 'CREATE_TODO':
 			TodoStore.addTodo(action.todo);
 			break;
-		case 'CREATE_TODO':
+			
+		case 'DELETE_TODO':
 			TodoStore.deleteTodo(action.id);
 			break;
 		default: return;
